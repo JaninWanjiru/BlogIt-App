4a4bf75381ac9f681c176f63cf22166f6a51d3f2
JaninWanjiru <janinndongo02@gmail.com>
2025-07-09 15:56:53 +0300

    add controllers, routes and middlewares folders to handle logic, endpoints and validations - add user registration logic in auth.contollers.ts - add endpoint to register and create a user in auth.route.ts file

---
+++ Server/src/controllers/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+const client = new PrismaClient()
+
+export const register = async (req:Request, res:Response) => {
+  try {
+    const {firstName, lastName, userName, email, password} = req.body;
+    const hashedPass = await bcrypt.hash(password, 9)
+    await client.user.create({
+        data: {firstName, lastName, userName, email, password: hashedPass}
+    })
+    res.status(201).json({ message: "User created successfully" });
+  } catch(e) {
+    res.status(500).json({message: "something went wrong"})
+  }
+};  
