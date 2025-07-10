46ae5132bc99a0f53c4e1a058db3be44ffab58e4
JaninWanjiru <janinndongo02@gmail.com>
2025-07-10 11:17:43 +0300

    add verifyUserInfo middleware

--- Server/src/controllers/auth.controller.ts
+++ Server/src/controllers/auth.controller.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
-const client = new PrismaClient()
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+const client = new PrismaClient();
 
-export const register = async (req:Request, res:Response) => {
+export const register = async (req: Request, res: Response) => {
   try {
-    const {firstName, lastName, userName, email, password} = req.body;
-    const hashedPass = await bcrypt.hash(password, 9)
+    const { firstName, lastName, userName, email, password } = req.body;
+    const hashedPass = await bcrypt.hash(password, 9);
     await client.user.create({
-        data: {firstName, lastName, userName, email, password: hashedPass}
-    })
+      data: { firstName, lastName, userName, email, password: hashedPass },
+    });
     res.status(201).json({ message: "User created successfully" });
-  } catch(e) {
-    res.status(500).json({message: "something went wrong"})
+  } catch (e) {
+    res.status(500).json({ message: "something went wrong" });
   }
-};  
+};
--- Server/src/routes/auth.route.ts
+++ Server/src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import verifyUserInfo from '../middleware/verifyUserInfo';
 import { register } from '../controllers/auth.controller';
 
 const router = Router();
 
-router.post('/register' , register);
+router.post('/register' ,verifyUserInfo , register);
 
 export default router;
