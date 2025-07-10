769823ff802aa174825c724b0e5b67fbe4fe12e6
JaninWanjiru <janinndongo02@gmail.com>
2025-07-10 11:51:36 +0300

    add middleware for verifying password strength

---
+++ Server/src/middleware/verifyPasswordStrength.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import zxcvbn from 'zxcvbn';
+
+function verifyPassStrength(req: Request, res: Response, next: NextFunction) {
+    const {password} = req.body;
+    const result = zxcvbn(password);
+
+  if (result.score < 3) {
+    res.status(400).json({
+      message: "Please use a stronger password"
+    })
+    return;
+  }
+  next();
+}
+
+export default verifyPassStrength; 
--- Server/src/routes/auth.route.ts
+++ Server/src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import verifyUserInfo from '../middleware/verifyUserInfo';
+import verifyPassStrength from '../middleware/verifyPasswordStrength';
 import { register } from '../controllers/auth.controller';
 
 const router = Router();
 
-router.post('/register' ,verifyUserInfo , register);
+router.post('/register' ,verifyUserInfo, verifyPassStrength, register);
 
 export default router;
